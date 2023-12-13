7dbed4ac33aa08100352bb939c81b9cec79be4d1
Romano Zumbe <rzumbe@gmail.com>
2023-12-13 15:21:14 +0100

    code hygiene

--- lua/yanki/init.lua
+++ lua/yanki/init.lua
@@ -13,250 +13,246 @@ M.yanks = {}
 -- Current put position in the yank history
 M.yankIndex = 1
 
+--- Initialize the plugin
+---@param parm table the settings for yanki
 function M.setup(parm)
-	M.settings = (parm or {})
+    M.settings = (parm or {})
 
-	-- Either start monitoring the system clipboard or add an autocommand
-	-- for yanking in neovim
-	if M.settings.system_clipboard and M.settings.system_clipboard.observe then
-		sysclip.ObserveSystemClipboard(
-			M.settings.system_clipboard.command,
-			M.settings.system_clipboard.args,
-			M.settings.system_clipboard.wait,
-			M.AddToHistory
-		)
-	end
+    -- Either start monitoring the system clipboard or add an autocommand
+    -- for yanking in neovim
+    if M.settings.system_clipboard and M.settings.system_clipboard.observe then
+        sysclip.ObserveSystemClipboard(M.settings.system_clipboard.command,
+                                       M.settings.system_clipboard.args,
+                                       M.settings.system_clipboard.wait,
+                                       M.AddToHistory)
+    end
 
-	if M.settings.system_clipboard.yank_observe then
-		-- create autocommand to react to yank actions
-		vim.api.nvim_exec(
-			[[
+    if M.settings.system_clipboard.yank_observe then
+        -- create autocommand to react to yank actions
+        vim.api.nvim_exec([[
       augroup WatchYankRegister
         autocmd!
         autocmd TextYankPost * lua require("yanki").OnTextYank()
       augroup END
-    ]],
-			false
-		)
-	end
+    ]], false)
+    end
 end
 
 --- Called whenever Text is yanked (yank/delete/replace) in nvim
 function M.OnTextYank()
-	-- get the contents of the default register
-	local yankedText = vim.fn.getreg('"', 1)
-	M.AddToHistory(yankedText)
+    -- get the contents of the default register
+    local yankedText = vim.fn.getreg('"', 1)
+    M.AddToHistory(yankedText)
 end
 
+--- Applies all active transformers to the given string and adds it to the history
+---@param text string the string to add to the history
 function M.AddToHistory(text)
-	local lines = {}
-	-- Whatever was yanked goes into the temporary history
-	table.insert(lines, text)
+    local lines = {}
+    -- Whatever was yanked goes into the temporary history
+    table.insert(lines, text)
 
-	-- If transformer functions are defined all active ones will be applied
-	-- to the yanked text in the order the appear in the array
-	for _, transformer in ipairs(M.settings.transformer or {}) do
-		if transformer.active then
-			local newLines = {}
-			for i = 1, #lines do
-				local transformedText = transformer.action(lines[i])
+    -- If transformer functions are defined all active ones will be applied
+    -- to the yanked text in the order the appear in the array
+    for _, transformer in ipairs(M.settings.transformer or {}) do
+        if transformer.active then
+            local newLines = {}
+            for i = 1, #lines do
+                local transformedText = transformer.action(lines[i])
 
-				-- The transformer can return a string or a table of strings
-				-- if it is a table, each string will go into the history as
-				-- its own entry
-				if type(transformedText) == "table" then
-					for _, v in ipairs(transformedText) do
-						table.insert(newLines, v)
-					end
-				elseif type(transformedText) == "string" then
-					table.insert(newLines, transformedText)
-				end
-			end
-			lines = newLines
-		end
-	end
+                -- NOTE:The transformer can return a string or a table of strings
+                -- if it is a table, each string will go into the history as
+                -- its own entry
+                if type(transformedText) == "table" then
+                    for _, v in ipairs(transformedText) do
+                        table.insert(newLines, v)
+                    end
+                elseif type(transformedText) == "string" then
+                    table.insert(newLines, transformedText)
+                end
+            end
+            lines = newLines
+        end
+    end
 
-	-- Insert all new lines into the actual yank history
-	for _, v in ipairs(lines) do
-		table.insert(M.yanks, v)
-	end
+    -- Insert all new lines into the actual yank history
+    for _, v in ipairs(lines) do table.insert(M.yanks, v) end
 end
 
 -- Select the next entry from the history and put it in the
 -- buffer at the current cursor position
 function M.PutNext()
-	if #M.yanks == 0 then
-		return
-	end
-	local nextPut = M.yanks[M.yankIndex]
-	M.Put(nextPut)
+    if #M.yanks == 0 then return end
+    local nextPut = M.yanks[M.yankIndex]
+    M.Put(nextPut)
 end
 
 -- Put the complete yank history in the current buffer
 function M.PutAll()
-	if #M.yanks == 0 then
-		return
-	end
-	for _, yank in ipairs(M.yanks) do
-		M.Put(yank)
-	end
+    if #M.yanks == 0 then return end
+    for _, yank in ipairs(M.yanks) do M.Put(yank) end
 end
 
 --- Put text into the active buffer at the current cursor position
 ---@param content string text to put into the buffer
 function M.Put(content)
-	-- get current cursor position
-	local currentLine = vim.fn.line(".")
-	local currentCol = vim.fn.col(".")
+    -- get current cursor position
+    local currentLine = vim.fn.line(".")
+    local currentCol = vim.fn.col(".")
 
-	-- HACK: if the text ends with a new line or it is explicitly configured
-	-- the text will be put in the next line. Mostly to mimic vims
-	-- default put behavior
-	if M.settings.PutInNewLine or string.sub(content, -1) == "\n" then
-		local lines = vim.fn.split(content, "\n")
+    -- HACK: if the text ends with a new line or it is explicitly configured
+    -- the text will be put in the next line. Mostly to mimic vims
+    -- default put behavior
+    if M.settings.PutInNewLine or string.sub(content, -1) == "\n" then
+        local lines = vim.fn.split(content, "\n")
 
-		vim.api.nvim_buf_set_lines(0, currentLine, currentLine, false, lines)
-	else
-		-- Otherwise just split the text into seperate lines and start puting
-		-- the lines at the current position
-		local lines = vim.split(content, "\n")
-		vim.api.nvim_buf_set_text(0, currentLine - 1, currentCol - 1, currentLine - 1, currentCol - 1, lines)
-	end
+        vim.api.nvim_buf_set_lines(0, currentLine, currentLine, false, lines)
+    else
+        -- Otherwise just split the text into seperate lines and start puting
+        -- the lines at the current position
+        local lines = vim.split(content, "\n")
+        vim.api.nvim_buf_set_text(0, currentLine - 1, currentCol - 1,
+                                  currentLine - 1, currentCol - 1, lines)
+    end
 
-	-- raise the index by one and start from the beginning after reaching
-	-- the last index
-	M.yankIndex = (M.yankIndex % #M.yanks) + 1
+    -- raise the index by one and start from the beginning after reaching
+    -- the last index
+    M.yankIndex = (M.yankIndex % #M.yanks) + 1
 end
 
 -- Clear the yank history to start over
 function M.ClearYankHistory()
-	M.yanks = {}
-	M.yankIndex = 1
+    M.yanks = {}
+    M.yankIndex = 1
 end
 
 --- Open telescope picker to inspect and manipulate the yank history
 ---@param opts table telescope picker options
 function M.ShowYankHistory(opts)
-	opts = opts or {}
+    opts = opts or {}
 
-	pickers
-		.new(opts, {
-			prompt_title = "Yank History",
-			finder = M.GetYankFinder(),
-			sorter = conf.generic_sorter(opts),
-			attach_mappings = function(prompt_bufnr, map)
-				-- the default action (return) is to put the text into the buffer
-				actions.select_default:replace(function()
-					actions.close(prompt_bufnr)
-					local selection = action_state.get_selected_entry()
-					M.Put(selection.ordinal)
-				end)
-				-- clear the yank history but only in normal mode
-				map({ "n" }, "<C-c>", function(_prompt_bufnr)
-					M.ClearYankHistory()
-					action_state.get_current_picker(_prompt_bufnr):refresh(M.GetYankFinder())
-				end)
-				-- set next element to put
-				map({ "i", "n" }, "<C-n>", function(_prompt_bufnr)
-					local selection = action_state.get_selected_entry()
-					M.yankIndex = selection.value[3]
-					action_state.get_current_picker(_prompt_bufnr):refresh(M.GetYankFinder())
-				end)
-				-- delete element from history
-				map({ "i", "n" }, "<C-d>", function(_prompt_bufnr)
-					local selection = action_state.get_selected_entry()
-					table.remove(M.yanks, selection.value[3])
-					action_state.get_current_picker(_prompt_bufnr):refresh(M.GetYankFinder())
-				end)
-				-- move element up in the history
-				map({ "i", "n" }, "<C-u>", function(_prompt_bufnr)
-					local selection = action_state.get_selected_entry()
-					M.yanks = util.Swap(M.yanks, selection.value[3], (selection.value[3] % #M.yanks) + 1)
-					action_state.get_current_picker(_prompt_bufnr):refresh(M.GetYankFinder())
-				end)
-				return true
-			end,
-		})
-		:find()
+    pickers.new(opts, {
+        prompt_title = "Yank History",
+        finder = M.GetYankFinder(),
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr, map)
+            -- the default action (return) is to put the text into the buffer
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                M.Put(selection.ordinal)
+            end)
+            -- clear the yank history but only in normal mode
+            map({"n"}, "<C-c>", function(_prompt_bufnr)
+                M.ClearYankHistory()
+                action_state.get_current_picker(_prompt_bufnr):refresh(
+                    M.GetYankFinder())
+            end)
+            -- set next element to put
+            map({"i", "n"}, "<C-n>", function(_prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                M.yankIndex = selection.value[3]
+                action_state.get_current_picker(_prompt_bufnr):refresh(
+                    M.GetYankFinder())
+            end)
+            -- delete element from history
+            map({"i", "n"}, "<C-d>", function(_prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                table.remove(M.yanks, selection.value[3])
+                action_state.get_current_picker(_prompt_bufnr):refresh(
+                    M.GetYankFinder())
+            end)
+            -- move element up in the history
+            map({"i", "n"}, "<C-u>", function(_prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                M.yanks = util.Swap(M.yanks, selection.value[3],
+                                    (selection.value[3] % #M.yanks) + 1)
+                action_state.get_current_picker(_prompt_bufnr):refresh(
+                    M.GetYankFinder())
+            end)
+            return true
+        end
+    }):find()
 end
 
 --- Open telescope picker to inspect and activate/deactivate transformers
 ---@param opts table telescope picker options
 function M.ShowTransformers(opts)
-	opts = opts or {}
+    opts = opts or {}
 
-	pickers
-		.new(opts, {
-			prompt_title = "Transformer",
-			finder = M.GetTransformerFinder(),
-			sorter = conf.generic_sorter(opts),
-			attach_mappings = function(prompt_bufnr, map)
-				-- the default action is to toggle the active state of the transformer
-				actions.select_default:replace(function()
-					local selection = action_state.get_selected_entry()
-					-- vim.api.nvim_put({selection[1]}, "", false, true)
-					M.settings.transformer[selection.index].active = not M.settings.transformer[selection.index].active
-					-- selection.value[1].active = true
-					action_state.get_current_picker(prompt_bufnr):refresh(M.GetTransformerFinder())
-				end)
-				-- move the transformer up in the execution order
-				map({ "i", "n" }, "<C-u>", function(_prompt_bufnr)
-					local selection = action_state.get_selected_entry()
-					M.settings.transformer = util.Swap(
-						M.settings.transformer,
-						selection.value[2],
-						(selection.value[2] % #M.settings.transformer) + 1
-					)
-					action_state.get_current_picker(_prompt_bufnr):refresh(M.GetTransformerFinder())
-				end)
-				return true
-			end,
-		})
-		:find()
+    pickers.new(opts, {
+        prompt_title = "Transformer",
+        finder = M.GetTransformerFinder(),
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr, map)
+            -- the default action is to toggle the active state of the transformer
+            actions.select_default:replace(function()
+                local selection = action_state.get_selected_entry()
+                -- vim.api.nvim_put({selection[1]}, "", false, true)
+                M.settings.transformer[selection.index].active = not M.settings
+                                                                     .transformer[selection.index]
+                                                                     .active
+                -- selection.value[1].active = true
+                action_state.get_current_picker(prompt_bufnr):refresh(
+                    M.GetTransformerFinder())
+            end)
+            -- move the transformer up in the execution order
+            map({"i", "n"}, "<C-u>", function(_prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                M.settings.transformer =
+                    util.Swap(M.settings.transformer, selection.value[2],
+                              (selection.value[2] % #M.settings.transformer) + 1)
+                action_state.get_current_picker(_prompt_bufnr):refresh(
+                    M.GetTransformerFinder())
+            end)
+            return true
+        end
+    }):find()
 end
 
 -- Create a finder for the transformer picker
 function M.GetTransformerFinder()
-	local transformerList = {}
+    local transformerList = {}
 
-	for i = 1, #M.settings.transformer do
-		table.insert(transformerList, { M.settings.transformer[i], i })
-	end
+    for i = 1, #M.settings.transformer do
+        table.insert(transformerList, {M.settings.transformer[i], i})
+    end
 
-	local finder = finders.new_table({
-		results = transformerList,
-		entry_maker = function(entry)
-			return {
-				value = entry,
-				display = entry[1].active and "*" .. entry[1].name or entry[1].name,
-				ordinal = entry[1].name,
-			}
-		end,
-	})
+    local finder = finders.new_table({
+        results = transformerList,
+        entry_maker = function(entry)
+            return {
+                value = entry,
+                display = entry[1].active and "*" .. entry[1].name or
+                    entry[1].name,
+                ordinal = entry[1].name
+            }
+        end
+    })
 
-	return finder
+    return finder
 end
 
 -- create a finder for the yank history picker
 function M.GetYankFinder()
-	local yankList = {}
+    local yankList = {}
 
-	for i = 1, #M.yanks do
-		table.insert(yankList, { M.yanks[i], i == M.yankIndex, i })
-	end
+    for i = 1, #M.yanks do
+        table.insert(yankList, {M.yanks[i], i == M.yankIndex, i})
+    end
 
-	local finder = finders.new_table({
-		results = yankList,
-		entry_maker = function(entry)
-			return {
-				value = entry,
-				display = entry[2] and "*" .. entry[1] or entry[1],
-				ordinal = entry[1],
-			}
-		end,
-	})
+    local finder = finders.new_table({
+        results = yankList,
+        entry_maker = function(entry)
+            return {
+                value = entry,
+                display = entry[2] and "*" .. entry[1] or entry[1],
+                ordinal = entry[1]
+            }
+        end
+    })
 
-	return finder
+    return finder
 end
 
 return M
--- lua/yanki/sysclip.lua
+++ lua/yanki/sysclip.lua
@@ -1,56 +1,56 @@
 ---@diagnostic disable: param-type-mismatch
 local uv = vim.loop
 local M = {}
-M.lastsysClip = ""
-M.timer = vim.loop.new_timer()
-M.lastHandle = nil
+local lastsysClip = ""
+local timer = vim.loop.new_timer()
 
+--- Starts the global timer to check for system clipboard changes
+---@param sysclipCommand string the application to check for system clipboard changes
+---@param args string arguments to the application
+---@param wait number how long to wait before rechecking
+---@param AddToHistory function the function to call when there is something new
 function M.ObserveSystemClipboard(sysclipCommand, args, wait, AddToHistory)
-	M.timer:start(0, wait, function()
-		M.GetClipboardText(
-			sysclipCommand,
-			args,
-			vim.schedule_wrap(function(data)
-				if data and data ~= M.lastsysClip then
-					M.lastsysClip = data
-					AddToHistory(data)
-				end
-			end)
-		)
-	end)
+    timer:start(0, wait, function()
+        M.GetStdoutFromAsync(sysclipCommand, args,
+        -- the function call needs to be wrapped to call vim.fn functions from the vim.loop
+                             vim.schedule_wrap(function(data)
+            if data and data ~= lastsysClip then
+                lastsysClip = data
+                AddToHistory(data)
+            end
+        end))
+    end)
 end
 
-function M.GetClipboardText(command, args, action)
-	local stdout = uv.new_pipe()
-	local handle, pid = uv.spawn(command, { stdio = { nil, stdout }, args = args }, function()
-		stdout:read_stop()
-		stdout:close()
---		handle:close()
-	end)
-	M.lastHandle = handle
-	uv.read_start(stdout, function(err, data)
-		assert(not err, err)
-		if data then
-			action(data)
-		else
-			-- if handle then
-			--     -- uv.shutdown(stdin, function() uv.close(handle) end)
-			--     if not uv.is_closing(handle) then uv.close(handle) end
-			-- end
-		end
-	end)
-	-- HACK: This throws a lot of errors, no idea what to do about it
-	-- uv.read_start(stderr, function(err, data)
-	--     assert(not err, err)
-	--     if data then
-	--         print("stderr chunk", stderr, data)
-	--     else
-	--         print("stderr end", stderr)
-	--     end
-	-- end)
+--- Calls an external command and runs a callback with the data returned on stdout
+---@param command string command to execute
+---@param args table arguments to the command
+---@param callback function callback function that will be executed with the data from stdout as parameter
+function M.GetStdoutFromAsync(command, args, callback)
+    local stdout = uv.new_pipe()
+    -- TODO: Checkout if the handle needs to get closed
+    local handle, _ = uv.spawn(command, {stdio = {nil, stdout}, args = args},
+    -- close the pipes after executing the command
+                               function()
+        if stdout then
+            stdout:read_stop()
+            stdout:close()
+        end
+    end)
 
-	-- if handle then uv.shutdown(stdin, function() uv.close(handle) end) end
-	-- if not uv.is_active(handle) then uv.close(handle) end
+    uv.read_start(stdout, function(err, data)
+        assert(not err, err)
+        if data then callback(data) end
+    end)
+    -- HACK: This throws a lot of errors, no idea what to do about it
+    -- uv.read_start(stderr, function(err, data)
+    --     assert(not err, err)
+    --     if data then
+    --         print("stderr chunk", stderr, data)
+    --     else
+    --         print("stderr end", stderr)
+    --     end
+    -- end)
 end
 
 return M
